44d1a9b59db7e23ec46ff086050133c9fb5c1f1b
lzhang100 <42689809+lzhang100@users.noreply.github.com>
2018-09-10 23:35:24 -0700

    commitoptionaluser

--- HelloLester/app/src/main/java/com/example/axess/hellolester/MainActivity.java
+++ HelloLester/app/src/main/java/com/example/axess/hellolester/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.axess.hellolester;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,5 +31,34 @@ public class MainActivity extends AppCompatActivity {
                         getResources().getColor(R.color.orange));
             }
         });
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newText=((EditText)findViewById(R.id.editText)).getText().toString();
+                if(TextUtils.isEmpty(newText)){
+                    ((TextView) findViewById(R.id.textView3)).setText("Hello from Lester");
+                }else {
+                    ((TextView) findViewById(R.id.textView3)).setText(newText);
+                }
+
+            }
+        });
+
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.textView3)).setTextColor(
+                        getResources().getColor(R.color.black));
+
+                findViewById(R.id.rootView).setBackgroundColor(
+                        getResources().getColor(R.color.colorAccent));
+
+                ((TextView) findViewById(R.id.textView3)).setText("Hello from Lester");
+
+            }
+        });
+
+
     }
 }
